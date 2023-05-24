e24f85b76576e6a667880634db8f33585c3252b3
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2023-05-24 19:53:59 -0300

    feat: get preco from elements

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
 const puppeteer = require('puppeteer');
 
+const url = "https://barcimoveis.com.br/busca/";
 const links = [
   {
+    slug: 'preco_medio_metro_quadrado',
+    link: url
+  },
+  {
+    slug: 'preco_medio_imovies',
+    link: url
+  },
+  {
+    slug: 'quantidade_imovies_total',
+    link: url
+  },
+  {
+    slug: 'quantidade_imovies_por_regiao',
+    link: url
+  },
+  {
+    slug: 'media_de_dormitorios',
+    link: url
+  },
+  {
     slug: 'preco_aluguel_medio',
-    link: 'https://valorinveste.globo.com/produtos/imoveis/noticia/2023/01/17/preco-de-aluguel-de-imoveis-tem-em-2022-maior-alta-em-11-anos.ghtml'
+    link: url
   },
   {
     slug: 'taxa_juros',
     link: 'https://blog.nubank.com.br/taxa-selic/'
   },
-  {
-    slug: 'preco_medio_metro_quadrado',
-    link: 'https://www.ibge.gov.br/estatisticas/economicas/precos-e-custos/9270-sistema-nacional-de-pesquisa-de-custos-e-indices-da-construcao-civil.html?=&t=destaques'
-  },
-  {
-    slug: 'percentual_imoveis_alugados_e_vendidos',
-    link: 'https://agenciabrasil.ebc.com.br/economia/noticia/2020-05/maioria-dos-brasileiros-mora-em-casa-e-e-dona-do-imovel-mostra-ibge#:~:text=Os%20im%C3%B3veis%20alugados%20representam%2018,%2C%20com%2073%2C6%25.'
-  },
-  {
-    slug: 'tempo_medio_de_vendas',
-    link: 'https://portal.loft.com.br/quanto-tempo-leva-vender-imovel/#:~:text=Vender%20um%20ap%C3%AA%20no%20Brasil,Associa%C3%A7%C3%A3o%20Brasileira%20de%20Incorporadoras%20Imobili%C3%A1rias).'
-  },
-  {
-    slug: 'numero_transacoes_imobiliarias',
-    link: 'https://www.registrodeimoveis.org.br/portal-estatistico-registral'
-  },
-  {
-    slug: 'todos_imoveis', // pegar valores, tipos, quantidades, ...
-    link: 'https://barcimoveis.com.br/busca/'
-  },
 ]
 
-(async () => {
+async function getElements()
+{
+  console.log(links[0]['link']);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.google.com');
-  const pageTitle = await page.title();
-  console.log(pageTitle);
-  await browser.close();
-})();
+  await page.goto(links[0]['link']);
+
+  const propertyList = await page.evaluate(() => {
+    const propertyContainer = document.getElementById('conteudoImoveis');
+    const propertyItems = Array.from(propertyContainer.getElementsByClassName('property-thumb-info'));
+    const properties = [];
+
+    propertyItems.forEach((item) => {
+      const preco = item.querySelector('.preco').textContent;
+
+      properties.push({ preco });
+    });
+
+    return properties;
+  });
+
+  console.log(propertyList);
+};
+
+getElements()
